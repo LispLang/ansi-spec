8ddddc96e9bb301a6d9b6dfa9c58e8fedec5bc0c
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-11 17:01:52 -0200

    Better preprocessing

--- src/preprocess.lisp
+++ src/preprocess.lisp
@@ -2,52 +2,73 @@
 
 (in-package :cl-ansi-spec)
 
-(defparameter +input-tag-regexp+ "\\input ([^ \\n]+)\\n"
-  "The regular expression used for the \input tag.")
+;;; Regex-based filtering
 
-(defun valid-input-p (file-name)
-  (and (not (search "fig" file-name))
-       (not (search ".tc" file-name))
-       (not (equal file-name "index.idx"))))
+(defparameter *filters* (list))
 
-(defun include-file (name)
-  "Load an included file."
-  (let ((input-pathname
-          (make-pathname :name name
-                         :type "tex"
-                         :defaults +tex-directory+)))
-    ;; Ignore figures
-    (if (valid-input-p name)
-        (progn
-          (log:info "Including path ~S" input-pathname)
-          (uiop:read-file-string input-pathname))
-        "")))
+(defmacro define-filter (regex replacement)
+  "Define a filter that replaces `regex` with `replacement`."
+  `(push
+    (lambda (string)
+      (cl-ppcre:regex-replace-all ,regex
+                                  string
+                                  ,replacement))
+    *filters*))
+
+(defmacro define-tag-filter (tag-name replacement)
+  `(define-filter (format nil "\\\\~A" ,tag-name) ,replacement))
+
+(defun filter (string)
+  (let ((filtered-string string))
+    (loop for filter in *filters* do
+      (setf filtered-string (funcall filter filtered-string)))
+    filtered-string))
+
+;; Quotes
+(define-filter "``" "\\quote{")
+(define-filter "''" "}")
+
+;; Simple removals
+(define-filter "\\\\input setup" "")
+(define-filter "\\\\%-*- Mode: TeX -*-" "")
+(define-tag-filter "bye" "")
+
+;;; More complex filtering
 
 (defun include-inputs (string)
   "Replace all instances of '\input file-name' with the contents of 'file-name.tex'."
-  (cl-ppcre:regex-replace-all +input-tag-regexp+
-                              string
-                              #'(lambda (match &rest regs)
-                                  (declare (ignore match))
-                                  (include-file (first regs)))
-                              :simple-calls t))
-
-(defparameter +comment-regexp+ "([^\\\\])%.*")
+  (flet ((valid-input-p (file-name)
+           (and (not (search "fig" file-name))
+                (not (search ".tc" file-name))
+                (not (equal file-name "index.idx"))))
+         (include-file (name)
+           (let ((input-pathname
+                   (make-pathname :name name
+                                  :type "tex"
+                                  :defaults +tex-directory+)))
+             ;; Ignore figures
+             (if (valid-input-p name)
+                 (progn
+                   (log:info "Including path ~S" input-pathname)
+                   (uiop:read-file-string input-pathname))
+                 ""))))
+    (cl-ppcre:regex-replace-all "\\input ([^ \\n]+)\\n"
+                                string
+                                #'(lambda (match &rest regs)
+                                    (declare (ignore match))
+                                    (include-file (first regs)))
+                                :simple-calls t)))
 
 (defun remove-comments (string)
   "Remove TeX comments from a string."
-  (cl-ppcre:regex-replace-all +comment-regexp+
+  (cl-ppcre:regex-replace-all "([^\\\\])%.*"
                               string
                               #'(lambda (match &rest regs)
                                   (declare (ignore match))
                                   (first regs))
                               :simple-calls t))
 
-(defun replace-quotes (string)
-  "Replace ``TeX quotes`` with <quote> tags."
-  (cl-ppcre:regex-replace-all "''"
-                              (cl-ppcre:regex-replace-all "``" string "\\quote{")
-                              "}"))
+;;; Simplify the \beginChapter thing
 
 (defparameter +begin-chapter-regexp+
   "\\\\beginchapter{([^}]+)}{([^}]+)}{([^}]+)}{([^}]+)}")
@@ -67,18 +88,9 @@
                                           (fourth regs)))
                               :simple-calls t))
 
-(defun remove-misc (string)
-  (cl-ppcre:regex-replace-all
-   "\\\\input setup"
-   (cl-ppcre:regex-replace-all "\\\\%-*- Mode: TeX -*-"
-                               string
-                               "")
-   ""))
-
 (defun preprocess (string)
-  (remove-misc
-   (expand-abbreviations
-    (simplify-begin-chapter
-     (replace-quotes
-      (remove-comments
-       (include-inputs string)))))))
+  (filter
+   (simplify-begin-chapter
+    (expand-abbreviations
+     (remove-comments
+      (include-inputs string))))))

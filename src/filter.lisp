0a6d75cb93265db028e0c7432c89b190ed9e78bb
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-11 15:23:00 -0200

    Some changes to the filters

--- src/filter.lisp
+++ src/filter.lisp
@@ -1,4 +1,26 @@
 (in-package :cl-ansi-spec)
 
+(defparameter *filters* (list))
+
+(defmacro define-simple-filter (tag-name replacement)
+  "A simple search->replace for TeX elements."
+  `(push
+    (lambda (string)
+      (cl-ppcre:regex-replace-all (format nil "\\\\~A" ,tag-name)
+                                  string
+                                  ,replacement))
+    *filters*))
+
+(defmacro define-null-filter (tag-name)
+  "A filter that erases everything it matches."
+  `(define-simple-filter ,tag-name ""))
+
+(define-null-filter "bye")
+(define-null-filter "endchapter")
+(define-null-filter "endSection")
+
 (defun filter (string)
-  string)
+  (let ((filtered-string string))
+    (loop for filter in *filters* do
+      (setf filtered-string (funcall filter filtered-string)))
+    filtered-string))

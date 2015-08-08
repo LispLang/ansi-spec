55ee407a950ed55614854b745667b00e7f29fc50
Fernando Borretti <eudoxiahp@gmail.com>
2015-08-08 20:48:16 -0300

    Add section emitters

--- src/emitters.lisp
+++ src/emitters.lisp
@@ -12,6 +12,8 @@
 
 ;;;; Sections
 
+;;; Chapters
+
 (define-emitter ("beginchapter")
                 :before "<chapter ")
 
@@ -33,3 +35,21 @@
 
 (define-emitter ("endchapter")
                 :before "</chapter>")
+
+;;; Sections
+
+(macrolet ((define-section-emitter (start-name end-name)
+             `(progn
+                (define-emitter (,start-name)
+                                :before "<section title=\""
+                                :after "\">")
+
+                (define-emitter (,end-name)
+                                :before "</section>")))
+           (define-all-section-emitters (&rest sub-strings)
+             `(progn
+                ,@(loop for sub-string in sub-strings collecting
+                    `(define-section-emitter
+                         ,(format nil "begin~ASection" sub-string)
+                         ,(format nil "end~ASection" sub-string))))))
+  (define-all-section-emitters "" "sub" "subsub" "subsubsub" "subsubsubsub"))

fe9d7eaef7986e5d925a15287771e7168aca2055
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-30 18:57:44 -0300

    Update modes

--- src/modes.lisp
+++ src/modes.lisp
@@ -1,29 +1,18 @@
 ;;;; Define parser modes
 (in-package :ansi-spec.traverse)
-#|
-(define-mode ("div" node pos :arity 0)
-  (declare (ignore node pos))
-  nil)
+
+(define-mode ("div"))
 
 (loop for column-count in (list "two" "three" "four" "five") do
   ;; Each 'display{two|three|four|five}' macro takes two arguments: A title and
   ;; the contents of the table. In the table contents, rows are separated by
   ;; '\cr' tags, and columns in each row are separated by ampersands (which were
   ;; converted to the '\ampersand' directive in a previus transform).
-  (define-mode ((concatenate 'string "display" column-count) node pos :arity 2)
-    (print node)
-    (cond
-      ((= pos 0)
-       ;; Node with the title
-       (output (format nil "<table>~%"))
-       (output "<title>"))
-      ((= pos 1)
-       (output (format nil "~%</title>~%"))
-       (output "<body>")
-       (output "</body>")))))
+  (define-mode ((concatenate 'string "display" column-count))
+    #'(lambda (node)
+        ;; Called on the title
+        (print node))))
 
-(define-mode ("bye" node pos :arity 0)
+(define-mode ("bye")
   ;; For some reason tex documents end with this crap
-  (declare (ignore node pos))
-  nil)
-|#
+  )

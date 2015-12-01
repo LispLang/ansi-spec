22d68f25e89067c2397dd5aa03ec8257a01834f1
Fernando Borretti <eudoxiahp@gmail.com>
2015-12-01 00:45:25 -0300

    Fix issue code

--- src/modes/setup-aux.lisp
+++ src/modes/setup-aux.lisp
@@ -1,2 +1,30 @@
 ;;;; setup-aux.tex
 (in-package :ansi-spec.traverse)
+
+(define-alias "sc" "scaled")
+
+;;; Issues
+;;;
+;;; References to issues look like this:
+;;;
+;;; \issue{name}
+;;; content
+;;; \endissue{name}
+;;;
+;;; This is to be interpreted as "[content] is caused by the issue [name]". In
+;;; CLHS, these references are collected and displayed as a list of issues at
+;;; the end of the page.
+
+(define-mode ("issue")
+  :callbacks
+  (((node)
+    (let ((text (elt (plump:children node) 0)))
+      (output (format nil "<issueref id=~S>" (plump:text text)))
+      (plump:remove-child text)))))
+
+(define-mode ("endissue")
+  :callbacks
+  (((node)
+    (let ((text (elt (plump:children node) 0)))
+      (plump:remove-child text))
+    (output "</issueref>"))))

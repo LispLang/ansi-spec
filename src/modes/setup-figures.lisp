5a0beabecf45f7713376794831ecf062d34e8d4f
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-30 23:34:06 -0300

    Add figure macros

---
+++ src/modes/setup-figures.lisp
@@ -0,0 +1,46 @@
+;;;; setup-figures.tex
+(in-package :ansi-spec.traverse)
+
+(define-alias "StdCharsThree" "Figure 2--5")
+
+(define-alias "PossibleSyntaxTypes" "Figure~2--6")
+
+(define-alias "CharSyntaxTypesInStdSyntax" "Figure~2--7")
+
+(define-alias "ConstituentTraitsOfStdChars" "Figure~2--8")
+
+(define-alias "SyntaxForNumericTokens" "Figure~2--9")
+
+(define-alias "CLSpecialOps" "Figure~3--2")
+
+(define-alias "TypeInfoXrefs" "Figure~4--1")
+
+(define-alias "StandardizedAtomicTypeSpecs" "Figure~4--2")
+
+(define-alias "StandardizedCompoundTypeSpecNames" "Figure~4--3")
+
+(define-alias "TypesAndDeclsNames" "Figure~4--5")
+
+(define-alias "StandardizedTypeSpecifierNames" "Figure~4--6")
+
+(define-alias "ObjectSystemClasses" "Figure 4--7")
+
+(define-alias "ClassTypeCorrespondence" "Figure 4--8")
+
+(define-alias "StdMethDefOps" "Figure 7--1")
+
+(define-alias "StandardizedConditionTypes" "Figure 9--1")
+
+(define-alias "SequenceFunctions" "Figure 17--1")
+
+(define-alias "PathnameCaseFuns" "Figure 19--2")
+
+(define-alias "InputStreamOps" "Figure 21--2")
+
+(define-alias "OutputStreamOps" "Figure 21--3")
+
+(define-alias "StandardizedStreamVars" "Figure 21--6")
+
+(define-alias "OpenOrClosedStreamOps" "Figure 21--7")
+
+(define-alias "StdPrinterControlVars" "Figure 22--1")
